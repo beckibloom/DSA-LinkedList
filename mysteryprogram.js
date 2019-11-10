f7dfab43cc336f47d7d21a95aed35ca6ba97f2ee
Becki Blumer <rblumer13@gmail.com>
2019-11-10 13:27:05 -0600

    Mystery program

---
+++ mysteryprogram.js
@@ -0,0 +1,24 @@
+//This function removes duplicates from the linked list
+
+function WhatDoesThisProgramDo(lst) {
+    //establishes first item of list
+    let current = lst.head;
+    //iterates through the list
+    while (current !== null) {
+        //keeping track of the current node
+        let newNode = current;
+        //check the next node
+        while (newNode.next !== null) {
+            //if the next node's value is equal to the current node's value
+            if (newNode.next.value === current.value) {
+                //set the value of the current node's 'next' to the 'next-next' node
+                newNode.next = newNode.next.next;
+            }
+            //else, the current node's 'next' will stay the same
+            else {
+                newNode = newNode.next;
+            }
+        }
+        current = current.next;
+    }
+}
